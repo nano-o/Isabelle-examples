0ea9853f7f14483154c3b12f87bc4bfb5515a52a
Giuliano Losa <giuliano@losa.fr>
2015-09-16 13:58:04 -0400

    Now using non-ASCII symbols.

--- Peterson.thy
+++ Peterson.thy
@@ -16,7 +16,7 @@ section {* Specification of the algorithm *}
 typedecl State
   -- "The state of the system"
 
-type_synonym 'a StateFun = "State => 'a" 
+type_synonym 'a StateFun = "State \<Rightarrow> 'a"
 -- "The type of variables"
 
 datatype label1 = p1_l1 | p1_l2 | p1_l3 | p1_l4 
@@ -32,61 +32,61 @@ consts
   flag2     ::       "bool StateFun"
   turn      ::       "Proc StateFun"
 
-definition unchanged :: "('a StateFun) => State => State => bool" where
-  "unchanged == % fun s s' . fun s = fun s'"
+definition unchanged :: "('a StateFun) \<Rightarrow> State \<Rightarrow> State \<Rightarrow> bool" where
+  "unchanged \<equiv> \<lambda> fun s s' . fun s = fun s'"
 
-definition transition :: "(State * State) set" where
-  "transition ==
+definition transition :: "(State \<times> State) set" where
+  "transition \<equiv>
       {(s,s').
-        (pc1 s = p1_l1 & pc1 s' = p1_l2 & flag1 s' = True & unchanged (% s . (pc2 s, flag2 s, turn s)) s s')
-        |(pc1 s = p1_l2 & pc1 s' = p1_l3 & turn s' = P2  & unchanged (% s .(pc2 s, flag1 s, flag2 s)) s s')
-        |(pc1 s = p1_l3 & pc1 s' = p1_l4 & (flag2 s = False | turn s = P1)  & unchanged (% s . (pc2 s, flag1 s, flag2 s, turn s)) s s')
-        |(pc1 s = p1_l4 & pc1 s' = p1_l1 & flag1 s' = False  & unchanged (% s . (pc2 s, flag1 s, flag2 s, turn s)) s s')
+        (pc1 s = p1_l1 \<and> pc1 s' = p1_l2 \<and> flag1 s' = True \<and> unchanged (\<lambda> s . (pc2 s, flag2 s, turn s)) s s')
+        \<or>(pc1 s = p1_l2 \<and> pc1 s' = p1_l3 \<and> turn s' = P2  \<and> unchanged (\<lambda> s .(pc2 s, flag1 s, flag2 s)) s s')
+        \<or>(pc1 s = p1_l3 \<and> pc1 s' = p1_l4 \<and> (flag2 s = False \<or> turn s = P1)  \<and> unchanged (\<lambda> s . (pc2 s, flag1 s, flag2 s, turn s)) s s')
+        \<or>(pc1 s = p1_l4 \<and> pc1 s' = p1_l1 \<and> flag1 s' = False  \<and> unchanged (\<lambda> s . (pc2 s, flag1 s, flag2 s, turn s)) s s')
 
-        |(pc2 s = p2_l1 & pc2 s' = p2_l2 & flag2 s' = True & unchanged (% s . (pc1 s, flag1 s, turn s)) s s')
-        |(pc2 s = p2_l2 & pc2 s' = p2_l3 & turn s' = P1 & unchanged (% s . (pc1 s, flag1 s, flag2 s)) s s')
-        |(pc2 s = p2_l3 & pc2 s' = p2_l4 & (flag1 s = False | turn s = P2) & unchanged (% s . (pc1 s, flag1 s, flag2 s, turn s)) s s')
-        |(pc2 s = p2_l4 & pc2 s' = p2_l1 & flag2 s' = False & unchanged (% s . (pc1 s, flag1 s, turn s)) s s')
+        \<or>(pc2 s = p2_l1 \<and> pc2 s' = p2_l2 \<and> flag2 s' = True \<and> unchanged (\<lambda> s . (pc1 s, flag1 s, turn s)) s s')
+        \<or>(pc2 s = p2_l2 \<and> pc2 s' = p2_l3 \<and> turn s' = P1 \<and> unchanged (\<lambda> s . (pc1 s, flag1 s, flag2 s)) s s')
+        \<or>(pc2 s = p2_l3 \<and> pc2 s' = p2_l4 \<and> (flag1 s = False \<or> turn s = P2) \<and> unchanged (\<lambda> s . (pc1 s, flag1 s, flag2 s, turn s)) s s')
+        \<or>(pc2 s = p2_l4 \<and> pc2 s' = p2_l1 \<and> flag2 s' = False \<and> unchanged (\<lambda> s . (pc1 s, flag1 s, turn s)) s s')
     }"
 
 definition initial :: "State set" where
-  "initial == {s.
+  "initial \<equiv> {s.
   pc1 s = p1_l1
-  & pc2 s = p2_l1
-  & flag1 s = False 
-  & flag2 s = False
-  & turn s = P1}"
+  \<and> pc2 s = p2_l1
+  \<and> flag1 s = False 
+  \<and> flag2 s = False
+  \<and> turn s = P1}"
 
 definition safe :: "State set" where
   -- "Definition of the safety property of the algorithm"
-  "safe == {s. ~ (pc1 s = p1_l4 & pc2 s = p2_l4) }"
+  "safe \<equiv> {s. \<not> (pc1 s = p1_l4 \<and> pc2 s = p2_l4) }"
 
 section {*Safety proof*}
 
-definition ind_inv :: "State set => bool" where
+definition ind_inv :: "State set \<Rightarrow> bool" where
 -- "The definition of an inductive state invariant"
-  "ind_inv p == initial <= p & (ALL s s'. (s : p & (s,s') : transition) --> s' : p)"
+  "ind_inv p \<equiv> initial \<subseteq> p \<and> (\<forall> s s'. (s : p \<and> (s,s') \<in> transition) \<longrightarrow> s' : p)"
 
 subsection {* Four invariants that imply the safety property *}
 
 definition inv1_p1 :: "State set" where
-  "inv1_p1 == {s. pc1 s : {p1_l2, p1_l3, p1_l4} --> (flag1 s = True)}"
+  "inv1_p1 \<equiv> {s. pc1 s : {p1_l2, p1_l3, p1_l4} \<longrightarrow> (flag1 s = True)}"
 
 definition inv1_p2 :: "State set" where
-  "inv1_p2 == {s. pc2 s : {p2_l2, p2_l3, p2_l4} --> (flag2 s = True)}"
+  "inv1_p2 \<equiv> {s. pc2 s : {p2_l2, p2_l3, p2_l4} \<longrightarrow> (flag2 s = True)}"
 
 definition inv2_p1 :: "State set" where
-  "inv2_p1 == {s. pc1 s = p1_l4 --> (flag2 s = False | turn s = P1 | pc2 s = p2_l2)}"
+  "inv2_p1 \<equiv> {s. pc1 s = p1_l4 \<longrightarrow> (flag2 s = False \<or> turn s = P1 \<or> pc2 s = p2_l2)}"
 
 definition inv2_p2 :: "State set" where
-  "inv2_p2 == {s. pc2 s = p2_l4 --> (flag1 s = False | turn s = P2 | pc1 s = p1_l2)}"
+  "inv2_p2 \<equiv> {s. pc2 s = p2_l4 \<longrightarrow> (flag1 s = False \<or> turn s = P2 \<or> pc1 s = p1_l2)}"
 
-lemma invariants_inductive: "ind_inv inv1_p1 & ind_inv inv2_p2 & ind_inv inv1_p2 & ind_inv inv2_p1"
+lemma invariants_inductive: "ind_inv inv1_p1 \<and> ind_inv inv2_p2 \<and> ind_inv inv1_p2 \<and> ind_inv inv2_p1"
   -- "Proof that the invariants are inductive"
 unfolding ind_inv_def initial_def inv1_p1_def inv1_p2_def inv2_p1_def inv2_p2_def transition_def unchanged_def 
 by auto
 
-theorem safety: "inv1_p1 Int inv1_p2 Int inv2_p1 Int inv2_p2 <= safe" 
+theorem safety: "inv1_p1 Int inv1_p2 Int inv2_p1 Int inv2_p2 \<subseteq> safe" 
   -- "Proof that the invariants imply the safety property"
 unfolding inv1_p1_def inv1_p2_def inv2_p1_def inv2_p2_def safe_def 
 by auto
